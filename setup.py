d3e8082e499915bd588532d0ff31ac48ecccc231
Yelyzaveta Velizhanina <yelyzaveta.velizhanina@ulb.be>
2022-03-30 15:12:13 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -5,21 +5,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytristan",
-    version="0.1",
+    version="0.1a",
     author="Yelyzaveta Velizhanina and Bernard Knaepen",
     author_email="velizhaninae@gmail.com",
     description="Differential operators in Python",
-    long_description="Differential operators in Python",
     url="https://github.com/YelyzavetaV/pytristan",
     project_urls={
         "Bug Tracker": "https://github.com/YelyzavetaV/pytristan/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    install_requires=["numpy"],
+    python_requires=">=3.8",
 )
