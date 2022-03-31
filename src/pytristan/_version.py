39e645b63377648f39decd82f10a1312497149b1
Yelyzaveta Velizhanina <yelyzaveta.velizhanina@ulb.be>
2022-03-31 15:06:43 +0200

    versioneer install

--- src/pytristan/_version.py
+++ src/pytristan/_version.py
@@ -42,7 +42,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "pytristan-"
     cfg.versionfile_source = "src/pytristan/_version.py"
     cfg.verbose = False
