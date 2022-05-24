5cc866fcde0cf8aa8376e2460a56ee3dfd167758
Yelyzaveta Velizhanina <yelyzaveta.velizhanina@ulb.be>
2022-05-24 14:25:28 +0200

    - Integrate finite difference from Kitten Tristan; - add accuracy as a param to Derivative; - Test mixed derivatives.

--- src/pytristan/_dmat.py
+++ src/pytristan/_dmat.py
@@ -1,13 +1,63 @@
 """Differential Matrix"""
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Union, Callable
 from operator import index
 import warnings
 import numpy as np
+from scipy.linalg import solve
+from scipy.sparse import diags
 from scipy.fft import fft, ifft
 from .matutils import nkron
 
-__all__ = ["cheb_diff_mat", "four_diff_mat", "default_discs", "Derivative", "NDDiffMat"]
+__all__ = [
+    "fd_coeffs",
+    "fd_diff_mat",
+    "cheb_diff_mat",
+    "four_diff_mat",
+    "default_discs",
+    "Derivative",
+    "NDDiffMat",
+]
+
+
+def fd_coeffs(order, stencil_len, stencil_shift=0):
+    pows = np.arange(stencil_len)
+    stencil = pows + stencil_shift
+    lhs = np.tile(stencil, (stencil_len, 1)) ** pows[:, np.newaxis]
+
+    rhs = np.zeros(stencil_len)
+    rhs[order] = np.math.factorial(order)
+
+    return solve(lhs, rhs)
+
+
+def fd_diff_mat(x, order, accuracy=2):
+    npts = len(x)
+    # TO FIX
+    h = x[1] - x[0]
+
+    stencil_len_central = 2 * ((order - 1) // 2) + 1 + accuracy
+    stencil_len = order + accuracy
+    stencil_shift = -(stencil_len_central - 1) // 2
+
+    if npts < stencil_len:
+        raise ValueError("Number of grid points too low for accuracy required")
+
+    # Build diagonal skeleton.
+    coeffs = fd_coeffs(order, stencil_len_central, stencil_shift)
+    mat = diags(
+        coeffs,
+        range(stencil_shift, stencil_len_central + stencil_shift),
+        shape=(npts, npts),
+    ).A
+
+    # Build top rows and bottoms rows
+    for i in range(0, -stencil_shift):
+        coeffs = fd_coeffs(order, stencil_len, -i)
+        mat[i, :stencil_len] = coeffs
+        mat[-i - 1, -stencil_len:] = np.flip(coeffs) * (-1) ** order
+
+    return mat / h**order
 
 
 def cheb_diff_mat(x, order):
@@ -62,7 +112,7 @@ def four_diff_mat(x, order):
     return ifft(col * fft(np.eye(nt))).T.real
 
 
-DEFAULT_DISCS = dict(chebyshev=cheb_diff_mat, fourier=four_diff_mat)
+DEFAULT_DISCS = dict(findiff=fd_diff_mat, cheb=cheb_diff_mat, four=four_diff_mat)
 
 
 def default_discs():
@@ -71,9 +121,11 @@ def default_discs():
 
 @dataclass
 class Derivative:
-    disc: Union[str, Callable]
+    axis: int
     order: int
-    axis: int = 0
+    disc: Union[str, Callable]
+    accuracy: Union[None, int] = None
+    kwargs: dict = field(default_factory=dict)
 
     def __post_init__(self):
         if not isinstance(self.disc, str) and not callable(self.disc):
@@ -109,7 +161,7 @@ class NDDiffMat(np.ndarray):
                 )
             axis, order = d.axis, d.order
             if order:
-                disc = d.disc
+                disc, accuracy, kwargs = d.disc, d.accuracy, d.kwargs
                 try:
                     orders[axis], discs[axis], f = (
                         (order, disc.__name__, disc)
@@ -121,7 +173,11 @@ class NDDiffMat(np.ndarray):
                         f"Unknown discretisation {disc}. Available default "
                         f"discretisations are {default_discs()}."
                     ) from e
-                mats[axis] = f(grid.axpoints(axis), order)
+                # If the user specified the accuracy, pass it as a keyword argument.
+                if accuracy is not None:
+                    kwargs["accuracy"] = accuracy
+
+                mats[axis] = f(grid.axpoints(axis), order, **kwargs)
             else:
                 warnings.warn(
                     f"Requested a 0-th-order derivative along axis {axis}.",
--- src/pytristan/tests/test_dmat.py
+++ src/pytristan/tests/test_dmat.py
@@ -1,13 +1,60 @@
 import pytest
-from pytristan import Derivative
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from pytristan import (
+    nkron,
+    cheb,
+    Grid,
+    fd_diff_mat,
+    cheb_diff_mat,
+    Derivative,
+    NDDiffMat,
+)
 
 
 def test_derivative_invalid_args():
     with pytest.raises(TypeError):
-        Derivative(disc=None, order=1.0)
+        Derivative(axis=0, order=1, disc=None)
 
     with pytest.raises(TypeError):
-        Derivative(disc="fourier", order=1.0)
+        Derivative(axis=0, order=1.0, disc="four")
 
     with pytest.raises(ValueError):
-        Derivative(disc="fourier", order=-1)
+        Derivative(axis=0, order=-1, disc="four")
+
+
+uniform_3D_grid = Grid.from_bounds([-1.0, 1.0, 16], [0.0, 1.0, 12], [0.0, 2.0, 10])
+mixed_non_period_2D_grid = Grid.from_bounds(
+    (-1.0, 1.0, 16), (0.0, 1.0, 12), axes=[0], mappers=[cheb]
+)
+
+
+def test_nd_diff_mat_mixed():
+    fd_d1x = fd_diff_mat(uniform_3D_grid.axpoints(0), 1)
+    fd_d1y = fd_diff_mat(uniform_3D_grid.axpoints(1), 1)
+    fd_d2xy = NDDiffMat(
+        uniform_3D_grid,
+        Derivative(disc="findiff", axis=0, order=1),
+        Derivative(disc="findiff", axis=1, order=1),
+    )
+    assert_array_almost_equal(
+        nkron(fd_d1x, fd_d1y, np.eye(uniform_3D_grid.npts[2])), fd_d2xy
+    )
+
+    fd_d3z = fd_diff_mat(uniform_3D_grid.axpoints(2), 3)
+    fd_d1xd3z = NDDiffMat(
+        uniform_3D_grid,
+        Derivative(disc="findiff", axis=0, order=1),
+        Derivative(disc="findiff", axis=2, order=3),
+    )
+    assert_array_almost_equal(
+        nkron(fd_d1x, np.eye(uniform_3D_grid.npts[1]), fd_d3z), fd_d1xd3z
+    )
+
+    cheb_d1x = cheb_diff_mat(mixed_non_period_2D_grid.axpoints(0), 1)
+    mixed_non_period_d2xy = NDDiffMat(
+        mixed_non_period_2D_grid,
+        Derivative(disc="cheb", axis=0, order=1),
+        Derivative(disc="findiff", axis=1, order=1),
+    )
+    assert_array_almost_equal(nkron(cheb_d1x, fd_d1y), mixed_non_period_d2xy)
